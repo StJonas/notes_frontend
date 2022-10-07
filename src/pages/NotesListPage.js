a36bb60e9df9cf71a760278b9ef3f5255479a7c3
stjonas <jonasstoegerer@yahoo.at>
2022-10-07 22:33:31 +0700

    notes list page and notes page, proxy url

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import ListItem from '../components/ListItem'
 
 const NotesListPage = () => {
-  return (
-    <div>
-        notes
+
+    let [notes, setNotes] = useState([])
+    useEffect(() =>{
+        getNote()
+    }, [])
+
+    let getNote = async () => {
+        let response = await fetch('/api/notes/')
+        let data = await response.json()
+        setNotes(data)
+    }
+
+    return (
+    <div className="notes-list">
+        {notes.map((note, index) =>(
+            //<h3 key={index}>{note.body}</h3>
+            <ListItem key={index} note={note}/>
+        ))}
     </div>
-  )
+    )
 }
 
 export default NotesListPage
